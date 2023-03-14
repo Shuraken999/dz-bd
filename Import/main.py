6fd25c2113fa61eb8a8bb56451197dde1a3800dd
Shuraken999 <Shuraken999@mail.ru>
2023-03-14 15:27:55 +0400

    Update files folder import

--- Import/main.py
+++ Import/main.py
@@ -1,10 +1,10 @@
-Import datetime
+import datetime
 
 from application.salary import calculate_salary
 from application.db.people import get_emloyees
+from dirty_main import *
 
 calculate_salary()
 get_emloyees()
-datetime.date()
-
-
+print('Today is ', datetime.date.today())
+print_info()
